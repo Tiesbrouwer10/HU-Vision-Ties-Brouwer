a72361c5013fa8bd1f88ce3a9f86523c558864e0
Mick <mickbos@hotmail.com>
2020-03-11 15:52:42 +0100

    Begin student grayscale

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,11 +1,25 @@
 #include "StudentPreProcessing.h"
+#include "ImageFactory.h"
 
 
-IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	IntensityImageStudent IntensityImage;
-	
-	
-	return nullptr;
+Intensity StudentPreProcessing::doGrayScaling(RGB pixel) const {
+	return(pixel.r + pixel.g + pixel.b) / 3;
+}
+
+IntensityImage* StudentPreProcessing::stepToIntensityImage(const RGBImage& image) const {
+	int height = image.getHeight();
+	int width = image.getWidth();
+	std::cout << "Student pre processing" << std::endl;
+	IntensityImage* IntensityImage_ = ImageFactory::newIntensityImage(width, height);
+	IntensityImage_->set(width, height);
+
+	for (size_t i = 0; i < width; i++) {
+		for (size_t j = 0; j < height; j++) {
+			IntensityImage_->setPixel(i, j, doGrayScaling(image.getPixel(i, j)));
+		}
+	}
+
+	return IntensityImage_;
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
